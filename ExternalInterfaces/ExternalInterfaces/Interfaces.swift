4a94af2f6b65600c38b111edd1877d820b72230c
Julio Guzman <julio.guzman@globant.com>
2017-11-08 20:33:30 -0600

    [Genio] Add Genio API Response

--- ExternalInterfaces/ExternalInterfaces/Interfaces.swift
+++ ExternalInterfaces/ExternalInterfaces/Interfaces.swift
@@ -8,33 +8,50 @@
 
 import Foundation
 
-struct Message {
-    let type: String //
-    let text: String //
-    let image: Data //?
-}
-
-struct Header {
-    let type: String //
-    let name: String //?
-    let text: String //
-    let image: Data //?
-}
-
-struct Footer {
-    let type: String //
-    let name: String //?
-    let text: String //
-}
-
 struct Item {
-    enum Element {
-        case header
-        case footer
-        case sentMessage
-        case receivedMessage
-    }
-    let type: Element
+    let type: String
     let text: String
+    let imageURL: String?
+    let name: String?
+}
+
+extension Item {
+    private struct Keys {
+        static let type = "type"
+        static let name = "name"
+        static let text = "text"
+        static let imageURL = "image"
+    }
+    
+    init?(JSON: AnyObject) {
+        guard let type = JSON[Keys.type] as? String,
+              let text = JSON[Keys.text] as? String else {
+            return nil
+        }
+        
+        self.type = type
+        self.name = JSON[Keys.name] as? String
+        self.text = text
+        self.imageURL = JSON[Keys.imageURL] as? String
+    }
+}
+
+struct GenioAPIResponse {
+    let items: [Item]
+}
+
+extension GenioAPIResponse {
+    private struct Keys {
+        static let items = "array"
+    }
+    
+    init?(JSON: AnyObject) {
+        guard let response = JSON[Keys.items] as? [[String: Any]] else {
+                return nil
+        }
+        self.items = response.flatMap({ (json) -> Item? in
+            return Item(JSON: json as AnyObject)
+        })
+    }
 }
 
---
+++ ExternalInterfaces/ExternalInterfacesTests/GenoiAPIResponseTests.swift
@@ -0,0 +1,45 @@
+//
+//  GenoiAPIResponseTests.swift
+//  ExternalInterfacesTests
+//
+//  Created by Julio Cesar Guzman Villanueva on 11/8/17.
+//  Copyright © 2017 Julio Cesar Guzman Villanueva. All rights reserved.
+//
+
+import XCTest
+@testable import ExternalInterfaces
+
+private extension JSONSerialization {
+    static func makeJSONDictionary(from bundle: Bundle, with name: String) -> [String: Any]? {
+        guard let JSONStateFile = bundle.path(forResource: name, ofType: "json"),
+            let jsonString = try? String(contentsOfFile: JSONStateFile),
+            let data = jsonString.data(using: .utf8),
+            let json = try? JSONSerialization.jsonObject(with: data) else {
+                XCTFail("JSON Initializing failed")
+                return nil
+        }
+        return json as? [String: Any]
+    }
+}
+
+class GenoiAPIResponseTests: XCTestCase {
+    
+    func testAPIResponseInitialization() {
+        let jsonFileName = "GenioAPIResponse"
+        let bundle = Bundle(for: type(of: self))
+        let jsonDictionary = JSONSerialization.makeJSONDictionary(from: bundle, with: jsonFileName)
+        let parsedObject = jsonDictionary as AnyObject
+        guard let response = GenioAPIResponse(JSON: parsedObject) else {
+            XCTFail()
+            return
+        }
+        guard let firstElement = response.items.first else {
+            XCTFail()
+            return
+        }
+        XCTAssert(firstElement.type == "header")
+        XCTAssert(firstElement.name == "Sample Header")
+        XCTAssert(firstElement.text == "Header")
+        XCTAssert(firstElement.imageURL == "https://s3-us-west-2.amazonaws.com/genio.images/!3pfnNgNaorfJRI1DQzuSOrOYepkKcqQh6VHBg7jTr_G90lp3ytW3WOHxx!s2Rn0.png")
+    }
+}
