b862c284e0688595ce91d1b9f4db5c1f6cfff3a4
Julio Guzman <julio.guzman@globant.com>
2017-12-01 16:44:51 -0600

    Add sale price and image url to liverpool api response

--- ExternalInterfaces/ExternalInterfaces/Genio API Entities/LiverpoolAPIResponseItem.swift
+++ ExternalInterfaces/ExternalInterfaces/Genio API Entities/LiverpoolAPIResponseItem.swift
@@ -10,6 +10,8 @@ import Foundation
 
 struct LiverpoolAPIResponseItem {
     let displayName: String
+    let salePrice: String
+    let imageURL: String
 }
 
 
--- ExternalInterfaces/ExternalInterfaces/Parsers/Item+Parser.swift
+++ ExternalInterfaces/ExternalInterfaces/Parsers/Item+Parser.swift
@@ -12,14 +12,22 @@ extension LiverpoolAPIResponseItem {
     private struct Keys {
         static let attributes = "attributes"
         static let displayName = "product.displayName"
+        static let salePrice = "sku.sale_Price"
+        static let imageURL = "sku.smallImage"
     }
     
     init?(JSON: AnyObject) {
         guard let attributes = JSON[Keys.attributes] as? [String: Any],
               let displayNames = attributes[Keys.displayName] as? [String],
-              let displayName = displayNames.first else {
+              let displayName = displayNames.first,
+              let salePrices = attributes[Keys.salePrice] as? [String],
+              let salePrice = salePrices.first,
+              let imageURLs = attributes[Keys.imageURL] as? [String],
+              let imageURL = imageURLs.first else {
                 return nil
         }
         self.displayName = displayName
+        self.salePrice = salePrice
+        self.imageURL = imageURL
     }
 }
