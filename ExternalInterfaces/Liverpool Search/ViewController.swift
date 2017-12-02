0c8f258122acb029c47482c1d2ba9a7070843bbe
Julio Guzman <julio.guzman@globant.com>
2017-12-01 18:00:16 -0600

    Add Ramreel view with Default support

--- ExternalInterfaces/Liverpool Search/ViewController.swift
+++ ExternalInterfaces/Liverpool Search/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import RAMReel
+import ExternalInterfaces
 
-class ViewController: UIViewController {
+extension DefaultsKeys {
+    static let searchedStrings = DefaultsKey<[String]>("searchedStrings")
+}
 
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    var dataSource: SimplePrefixQueryDataSource!
+    var ramReel: RAMReel<RAMCell, UITextField, SimplePrefixQueryDataSource>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dataSource = SimplePrefixQueryDataSource(Defaults[.searchedStrings])
+        ramReel = make(with: dataSource)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private func make(with dataSource: SimplePrefixQueryDataSource) -> RAMReel<RAMCell, UITextField, SimplePrefixQueryDataSource> {
+        self.ramReel?.view.removeFromSuperview()
+        self.ramReel = nil
+        let frame = view.frame
+        let ramReel = RAMReel<RAMCell, UITextField, SimplePrefixQueryDataSource>(frame: frame,
+                                                                   dataSource: dataSource,
+                                                                   placeholder: "Busqueda",
+                                                                   attemptToDodgeKeyboard: true,
+                                                                   hook: nil)
+        ramReel.textFieldDelegate = self
+        ramReel.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(ramReel.view)
+        return ramReel
+    }
+    
+    public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        if let text = textField.text {
+            var cambio = Defaults[.searchedStrings]
+            cambio.append(text)
+            Defaults[.searchedStrings] = cambio
+            dataSource = SimplePrefixQueryDataSource(Defaults[.searchedStrings])
+            ramReel = make(with: dataSource)
+        }
+    }
 }
 
